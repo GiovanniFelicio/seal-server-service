1728e38d3154b65fbd90894c7cce9b1ffb5f257e
Giovanni <gvnnfelicio@gmail.com>
2021-07-04 13:16:09 -0300

    refatorando pacotes

--- src/main/java/com/seal/vtex/server/ServerApplication.java
+++ src/main/java/com/seal/vtex/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.omega.server;
+package com.seal.vtex.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/seal/vtex/server/ServerApplicationTests.java
+++ src/test/java/com/seal/vtex/server/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.omega.server;
+package com.seal.vtex.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
